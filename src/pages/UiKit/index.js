45b44b89faa89010d7746fea9f3c4e71e3d77d10
Pedro Ferreira <pedrovictorfe@gmail.com>
2020-03-21 18:11:19 -0300

    adicionando exemplos uikit

--- src/pages/UiKit/index.js
+++ src/pages/UiKit/index.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import { ExpandableBox, Input } from './components'
+import { Search } from '@styled-icons/evil/Search'
 
 const AllComponents = () => {
-    return (
-      <div style={{  width: '100%',}} >
-        <Input 
-            placeholder={"Teste placeholder"} 
-            onChange={(e) => console.log(e.target.value)}    
-        />
-        <ExpandableBox header={<div>Header works</div>} body={<div>Body works</div>} />
-      </div>
-    );
-  };
-  
-  export default AllComponents;
-  
+  return (
+    <div style={{ width: '100%', }} >
+      <Input
+        placeholder={"Teste placeholder"}
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <br />
+      <Input
+        icon={<Search color={"white"} size={"24px"} />}
+        placeholder={"Exemplo com Icone"}
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <br />
+      <Input
+        closeIcon
+        placeholder={"Exemplo com close Icon"}
+        onCloseClick={() => alert("Close clicked")}
+        onChange={(e) => console.log(e.target.value)}
+      />
+
+      <br />
+      <ExpandableBox header={<div>Header works</div>} headerExpaned={<div>Header Expaned works</div>}  body={<div>Body works</div>} />
+    </div>
+  );
+};
+
+export default AllComponents;
