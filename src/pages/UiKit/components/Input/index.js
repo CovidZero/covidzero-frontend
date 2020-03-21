03acd73fcdfbe22a760a067527a8be92ac71d96e
Pedro Ferreira <pedrovictorfe@gmail.com>
2020-03-21 18:11:10 -0300

    ajustes ui kit

--- src/pages/UiKit/components/Input/index.js
+++ src/pages/UiKit/components/Input/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { ContainerBox, Input as InputStyle, InputIcon } from './styles'
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
 
 const Input = (props) => {
   return (
     <ContainerBox>
-      { props.icon && <InputIcon></InputIcon>}
+      {props.icon && <InputIcon>{props.icon}</InputIcon>}
       <InputStyle {...props} />
+      {props.closeIcon &&
+        <CloseOutline onClick={() => props.onCloseClick && props.onCloseClick()} color={"white"} size={"24px"} />
+      }
     </ContainerBox>
   );
 };
