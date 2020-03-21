1deaa1f24ccad893fa2c2801f645bb8cda2875d9
qnedy <altmaisefequatro@gmail.com>
2020-03-21 20:16:43 -0300

    adicionando buttons no uikit

--- src/pages/UiKit/index.js
+++ src/pages/UiKit/index.js
@@ -1,56 +1,37 @@
 import React from 'react'
-<<<<<<< HEAD
 import { ExpandableBox, Input, Buttons } from './components'
-
-const AllComponents = () => {
-    return (
-      <>
-        <div style={{  width: '100%',}} >
-          <Input 
-              placeholder={"Teste placeholder"} 
-              onChange={(e) => console.log(e.target.value)}    
-          />
-          <ExpandableBox header={<div>Header works</div>} body={<div>Body works</div>} />
-        </div>
-        <div>
-          <Buttons />
-        </div>
-      </>
-    );
-  };
-  
-  export default AllComponents;
-  
-=======
-import { ExpandableBox, Input } from './components'
 import { Search } from '@styled-icons/evil/Search'
 
 const AllComponents = () => {
   return (
-    <div style={{ width: '100%', }} >
-      <Input
-        placeholder={"Teste placeholder"}
-        onChange={(e) => console.log(e.target.value)}
-      />
-      <br />
-      <Input
-        icon={<Search color={"white"} size={"24px"} />}
-        placeholder={"Exemplo com Icone"}
-        onChange={(e) => console.log(e.target.value)}
-      />
-      <br />
-      <Input
-        closeIcon
-        placeholder={"Exemplo com close Icon"}
-        onCloseClick={() => alert("Close clicked")}
-        onChange={(e) => console.log(e.target.value)}
-      />
+    <>
+      <div style={{ width: '100%', }} >
+        <Input
+          placeholder={"Teste placeholder"}
+          onChange={(e) => console.log(e.target.value)}
+        />
+        <br />
+        <Input
+          icon={<Search color={"white"} size={"24px"} />}
+          placeholder={"Exemplo com Icone"}
+          onChange={(e) => console.log(e.target.value)}
+        />
+        <br />
+        <Input
+          closeIcon
+          placeholder={"Exemplo com close Icon"}
+          onCloseClick={() => alert("Close clicked")}
+          onChange={(e) => console.log(e.target.value)}
+        />
 
-      <br />
-      <ExpandableBox header={<div>Header works</div>} headerExpaned={<div>Header Expaned works</div>}  body={<div>Body works</div>} />
-    </div>
+        <br />
+        <ExpandableBox header={<div>Header works</div>} headerExpaned={<div>Header Expaned works</div>}  body={<div>Body works</div>} />
+      </div>
+      <div>
+        <Buttons />
+      </div>
+    </>
   );
 };
 
 export default AllComponents;
->>>>>>> cff839694202eeaa24b87b4227fc896798c162c3
