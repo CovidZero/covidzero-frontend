f9b69f867c5070f9e8dee0c9a5591c9ebfe13e2f
Sérgio Danilo <sergioholanda@MacBook-Pro-de-Sergio.local>
2020-03-22 16:12:04 +0000

    refact: moving folder ./src/pages/components to ./src/components

--- src/pages/UiKit/index.js
+++ src/pages/UiKit/index.js
@@ -1,59 +1,79 @@
-import React from 'react'
-import { ExpandableBox, Input, Buttons, CardStats, CardNews } from './components'
-import { Search } from '@styled-icons/evil/Search'
-
+import React from "react";
+import {
+  ExpandableBox,
+  Input,
+  Buttons,
+  CardStats,
+  CardNews
+} from "~/components";
+import { Search } from "@styled-icons/evil/Search";
 
 const AllComponents = () => {
-  
-  const longContentCardNews = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+  const longContentCardNews =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
   return (
-    <div style={{ width: '100%' }} >
+    <div style={{ width: "100%" }}>
       <Input
         placeholder={"Teste placeholder"}
-        onChange={(e) => console.log(e.target.value)}
+        onChange={e => console.log(e.target.value)}
       />
       <br />
       <Input
         icon={<Search color={"white"} size={"24px"} />}
         placeholder={"Exemplo com Icone"}
-        onChange={(e) => console.log(e.target.value)}
+        onChange={e => console.log(e.target.value)}
       />
       <br />
       <Input
         closeIcon
         placeholder={"Exemplo com close Icon"}
         onCloseClick={() => alert("Close clicked")}
-        onChange={(e) => console.log(e.target.value)}
+        onChange={e => console.log(e.target.value)}
       />
 
       <br />
-      <ExpandableBox header={<div>Header works</div>} headerExpaned={<div>Header Expaned works</div>}  body={<div>Body works</div>} />
+      <ExpandableBox
+        header={<div>Header works</div>}
+        headerExpaned={<div>Header Expaned works</div>}
+        body={<div>Body works</div>}
+      />
 
       <br />
-      <CardStats title={<div>Title</div>} count="{quantity}"/>
+      <CardStats title={<div>Title</div>} count="{quantity}" />
       <br />
-      <CardStats status="confirmed" title={<div>Confirmados</div>} count="{quantity}"/>
+      <CardStats
+        status="confirmed"
+        title={<div>Confirmados</div>}
+        count="{quantity}"
+      />
       <br />
-      <CardStats status="recovered" title={<div>Recuperados</div>} count="{quantity}"/>
+      <CardStats
+        status="recovered"
+        title={<div>Recuperados</div>}
+        count="{quantity}"
+      />
       <br />
-      <CardStats status="suspect" title={<div>Suspeitos</div>} count="{quantity}"/>
+      <CardStats
+        status="suspect"
+        title={<div>Suspeitos</div>}
+        count="{quantity}"
+      />
       <br />
-      <CardStats status="death" title={<div>Óbitos</div>} count="{quantity}"/>
-      <br/>
-      <CardNews title="Como os gestores devem se comportar na crise Coronavírus?"
-                content={longContentCardNews}
-                date={new Date()}
-                tag={<div>MAIN_TAG</div>} />
-      <br/>
-                        
+      <CardStats status="death" title={<div>Óbitos</div>} count="{quantity}" />
+      <br />
+      <CardNews
+        title="Como os gestores devem se comportar na crise Coronavírus?"
+        content={longContentCardNews}
+        date={new Date()}
+        tag={<div>MAIN_TAG</div>}
+      />
+      <br />
+
       <div>
         <Buttons />
       </div>
-                     
-      
     </div>
-                  
   );
 };
 
