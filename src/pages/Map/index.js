f574f37f6468656473e703c6fdf8d4ad430886a1
Bruno <bruno@aptasd.com.br>
2020-03-22 01:38:22 -0300

    feat: added hook usePosition

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import usePosition from '~/services/usePosition';
+
 
 export default function Map() {
   const [t] = useTranslation();
-  return <h1 style={{ textAlign: "center" }}>{t("menu.map")}</h1>;
+  const { latitude, longitude, error } = usePosition();
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>{t("menu.map")}</h1>
+      <h3>Localização</h3>
+      <div>
+        {error
+          ? (<p>error: {error}</p>)
+          : (
+            <>
+              <p>latitude: {latitude}<br /></p>
+              <p>longitude: {longitude}<br /></p>
+            </>
+          )
+        }
+      </div>
+    </>
+  )
+
 }
