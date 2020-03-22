e559054dcb562481bdc4fc5be8430ee0e8fc97c8
Bueno <matheusbueno1063@gmail.com>
2020-03-22 13:35:13 -0300

    feat: add ubuntu font

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ import store from "./store/store";
 import history from "./services/history";
 import "./i18n";
 import theme from "./styles/theme";
+import GlobalFonts from "./styles/fonts";
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
+      <GlobalFonts />
       <ThemeProvider theme={theme}>
         <Suspense fallback={<div>Loading...</div>}>
           <App />
