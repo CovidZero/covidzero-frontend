06d0a14bd84ffb3c7d31a6e10f998a7f9039f7aa
Danilo <dpaduaoliveira@gmail.com>
2020-03-22 14:45:00 -0300

    Adicionado ArticleNewsHeader
    Adicionado ArticleNewsHeader

--- src/pages/UiKit/components/index.js
+++ src/pages/UiKit/components/index.js
@@ -2,10 +2,12 @@ import ExpandableBox from './ExpandableBox'
 import Input from './Input'
 import CardStats from './CardStats'
 import CardNews from './CardNews'
+import ArticleNewsHeader from './ArticleNewsHeader'
 
 export {
     Input,
     ExpandableBox,
     CardStats,
-    CardNews
+    CardNews,
+    ArticleNewsHeader
 }
--- src/pages/UiKit/index.js
+++ src/pages/UiKit/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { ExpandableBox, Input, CardStats, CardNews } from './components'
+import { ExpandableBox, Input, CardStats, CardNews, ArticleNewsHeader } from './components'
 import { Search } from '@styled-icons/evil/Search'
+import ImageArticle from '~/assets/images/img-reuniao.jpg'
 
 
 const AllComponents = () => {
@@ -26,26 +27,35 @@ const AllComponents = () => {
         onCloseClick={() => alert("Close clicked")}
         onChange={(e) => console.log(e.target.value)}
       />
-
       <br />
+      
       <ExpandableBox header={<div>Header works</div>} headerExpaned={<div>Header Expaned works</div>}  body={<div>Body works</div>} />
-
       <br />
+      
       <CardStats title={<div>Title</div>} count="{quantity}"/>
       <br />
+      
       <CardStats status="confirmed" title={<div>Confirmados</div>} count="{quantity}"/>
       <br />
+      
       <CardStats status="recovered" title={<div>Recuperados</div>} count="{quantity}"/>
       <br />
+      
       <CardStats status="suspect" title={<div>Suspeitos</div>} count="{quantity}"/>
       <br />
+      
       <CardStats status="death" title={<div>Óbitos</div>} count="{quantity}"/>
       <br/>
+      
       <CardNews title="Como os gestores devem se comportar na crise Coronavírus?"
                 content={longContentCardNews}
                 date={new Date()}
                 tag={<div>MAIN_TAG</div>} />
-      <br/><br/><br/><br/>
+      <br/>
+
+      <ArticleNewsHeader source={ImageArticle} title={<div>Como os gestores devem se comportar na crise Coronavírus?</div>}/>
+      <br/><br/><br/><br/><br/>
+      
       
     </div>
   );
