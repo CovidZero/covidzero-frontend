a65b0fe029500c26630fabe4e5be90b60cb6d979
Breno Sapucaia <brenoss.magalhaes@outlook.com>
2020-03-24 17:55:35 -0300

    layout da tela de news reconfigurado de acordo com a issue #73

--- src/pages/News/NewsItem.js
+++ src/pages/News/NewsItem.js
@@ -23,79 +23,76 @@ const NewsItem = props => {
       />
       <Grid className="main-content-grid">
         <Row>
-          <Cell desktopColumns={8} tabletColumns={8} phoneColumns={4}>
+          <Cell desktopColumns={12} tabletColumns={8} phoneColumns={4}>
+            <div className="news-title">
+              <h1>{`Como gestores devem se comportar na crise proporcionada pela pandemia? `}</h1>
+            </div>
+          </Cell>
+          <Cell className="news-info" desktopColumns={12} tabletColumns={8} phoneColumns={4}>
+            <div className="group-info">
+              <div className="news-date">{`{DD / MM / YYYY, HH/mm/SS}`}</div>
+              <div className="news-author">{`{AUTHOR_TAG}`} </div>
+            </div>
+            <a href="#" className=" share-button">
+              <img src={ShareIcon}></img>
+              <span> COMPARTILHAR </span>
+            </a>
+          </Cell>
+
+          <Cell desktopColumns={12} tabletColumns={8} phoneColumns={4}>
             <div className="news-item-header-image">
               <img
                 className="news-item-image"
                 src={require("~/assets/images/meeting_room.jpg")}
               />
-              <div className="news-item-title">Centered</div>
             </div>
           </Cell>
         </Row>
         <Row>
-          <Cell desktopColumns={6} tabletColumns={4} phoneColumns={2}>
-            <div className="news-info">{`Publicado por: {AUTHOR}`} </div>
+          <Cell desktopColumns={12} tabletColumns={8} phoneColumns={4} className="news-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              et efficitur mauris. Nunc elit lorem, rutrum in vulputate sed,
+              convallis ac quam. Nunc dictum ac magna vel porta. Suspendisse
+              quis magna at augue faucibus lacinia ut dapibus libero. Cras id
+              nibh a urna pharetra scelerisque. Sed nec malesuada magna. Nulla
+              neque massa, consectetur nec ipsum sit amet, varius pretium mi.
+              Mauris id felis accumsan, pharetra ligula a, viverra ipsum.
+              Maecenas eget iaculis nisl. Donec et nibh dapibus, dapibus nisi
+              et, aliquet risus. Sed dui nunc, mattis nec porttitor gravida,
+              feugiat non lacus.
+              </p>
           </Cell>
-          <Cell desktopColumns={6} tabletColumns={4} phoneColumns={2}>
-            <div className="news-info news-date">{`{DD / MM / YYYY, TIME}`}</div>
+          <Cell desktopColumns={12} tabletColumns={8} phoneColumns={4} className="news-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              et efficitur mauris. Nunc elit lorem, rutrum in vulputate sed,
+              convallis ac quam. Nunc dictum ac magna vel porta. Suspendisse
+              quis magna at augue faucibus lacinia ut dapibus libero. Cras id
+              nibh a urna pharetra scelerisque. Sed nec malesuada magna. Nulla
+              neque massa, consectetur nec ipsum sit amet, varius pretium mi.
+              Mauris id felis accumsan, pharetra ligula a, viverra ipsum.
+              Maecenas eget iaculis nisl. Donec et nibh dapibus, dapibus nisi
+              et, aliquet risus. Sed dui nunc, mattis nec porttitor gravida,
+              feugiat non lacus.
+              </p>
           </Cell>
-        </Row>
-        <Row>
-          <Cell desktopColumns={12} tabletColumns={8} phoneColumns={4}>
-            <div className="news-info news-content">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                et efficitur mauris. Nunc elit lorem, rutrum in vulputate sed,
-                convallis ac quam. Nunc dictum ac magna vel porta. Suspendisse
-                quis magna at augue faucibus lacinia ut dapibus libero. Cras id
-                nibh a urna pharetra scelerisque. Sed nec malesuada magna. Nulla
-                neque massa, consectetur nec ipsum sit amet, varius pretium mi.
-                Mauris id felis accumsan, pharetra ligula a, viverra ipsum.
-                Maecenas eget iaculis nisl. Donec et nibh dapibus, dapibus nisi
-                et, aliquet risus. Sed dui nunc, mattis nec porttitor gravida,
-                feugiat non lacus.
+          <Cell desktopColumns={12} tabletColumns={8} phoneColumns={4} className="news-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              et efficitur mauris. Nunc elit lorem, rutrum in vulputate sed,
+              convallis ac quam. Nunc dictum ac magna vel porta. Suspendisse
+              quis magna at augue faucibus lacinia ut dapibus libero. Cras id
+              nibh a urna pharetra scelerisque. Sed nec malesuada magna. Nulla
+              neque massa, consectetur nec ipsum sit amet, varius pretium mi.
+              Mauris id felis accumsan, pharetra ligula a, viverra ipsum.
+              Maecenas eget iaculis nisl. Donec et nibh dapibus, dapibus nisi
+              et, aliquet risus. Sed dui nunc, mattis nec porttitor gravida,
+              feugiat non lacus.
               </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                et efficitur mauris. Nunc elit lorem, rutrum in vulputate sed,
-                convallis ac quam. Nunc dictum ac magna vel porta. Suspendisse
-                quis magna at augue faucibus lacinia ut dapibus libero. Cras id
-                nibh a urna pharetra scelerisque. Sed nec malesuada magna. Nulla
-                neque massa, consectetur nec ipsum sit amet, varius pretium mi.
-                Mauris id felis accumsan, pharetra ligula a, viverra ipsum.
-                Maecenas eget iaculis nisl. Donec et nibh dapibus, dapibus nisi
-                et, aliquet risus. Sed dui nunc, mattis nec porttitor gravida,
-                feugiat non lacus.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                et efficitur mauris. Nunc elit lorem, rutrum in vulputate sed,
-                convallis ac quam. Nunc dictum ac magna vel porta. Suspendisse
-                quis magna at augue faucibus lacinia ut dapibus libero. Cras id
-                nibh a urna pharetra scelerisque. Sed nec malesuada magna. Nulla
-                neque massa, consectetur nec ipsum sit amet, varius pretium mi.
-                Mauris id felis accumsan, pharetra ligula a, viverra ipsum.
-                Maecenas eget iaculis nisl. Donec et nibh dapibus, dapibus nisi
-                et, aliquet risus. Sed dui nunc, mattis nec porttitor gravida,
-                feugiat non lacus.
-              </p>
-            </div>
           </Cell>
         </Row>
       </Grid>
-      <Grid className="news-item-header">
-
-      </Grid>
-
-      <Grid>
-       
-      </Grid>
-
-      <Grid>
-        
-      </Grid>
-
     </>
   );
 };
