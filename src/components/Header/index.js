3c16e9fda9ed71536d1a8ec0567ba1c3a65042a5
Sérgio Danilo <sergioholanda@MacBook-Pro-de-Sergio.local>
2020-03-24 14:53:36 +0000

    feature: creating news item page and styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,11 +4,11 @@ import LogoSvg from "~/assets/icons/small-log.svg";
 import Notification from "~/assets/icons/bell.svg";
 import { Container, AlignItem, Title, Icon, LeftIcon } from "./styles";
 
-const Header = ({ leftIcon, title, rightIcon }) => {
+const Header = ({ leftIcon, title, rightIcon, actionLeftIcon }) => {
   return (
     <Container>
       <AlignItem>
-        <LeftIcon src={leftIcon || LogoSvg} />
+        <LeftIcon src={leftIcon || LogoSvg} onClick={actionLeftIcon} />
         <Title>{title}</Title>
       </AlignItem>
       <Icon src={rightIcon || Notification} />
--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -14,6 +14,7 @@ export default function News() {
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
   const [t] = useTranslation();
+
   return (
     <>
       <Header title={t("header.news")} rightIcon={ShareIcon} />
