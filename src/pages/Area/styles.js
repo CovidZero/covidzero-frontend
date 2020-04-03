aed7ea2b9b634b3a8b7473ccd82c522bf2b7735c
Pablo Klein <pablo@pabloklein.com.br>
2020-04-03 19:36:10 +0100

    height adjustment for specific iphone devices

--- src/pages/Area/styles.js
+++ src/pages/Area/styles.js
@@ -2,9 +2,25 @@ import styled from "styled-components";
 
 export const ContainerMap = styled.div`
     width: 100%;
-    height: calc(100vh - 80px);
+    height: 93vh;
 
     @media (max-width: 600px){
+      height: 77vh;
+    }
+
+    /* iPhone X , XS, 11 Pro */
+    @media only screen and (min-device-width: 375px) and (max-device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+      height: 71vh;
+    }
+
+    /* iPhone XR, 11 */
+    @media only screen and (min-device-width : 414px) and (max-device-height : 896px) and (-webkit-device-pixel-ratio : 2) {
+      height: 74vh;
+    }
+
+    /* iPhone XS Max, 11 Pro Max */
+    @media only screen and (min-device-width : 414px) and (max-device-height : 896px) and (-webkit-device-pixel-ratio : 3) {
+      height: 75vh;
     }
 `;
 
